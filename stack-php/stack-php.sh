f0e99560cd9539e97df3518eada9cc6cee0acf3d
mlamamra <mlamamra@dawan.fr>
2025-09-30 08:31:45 +0200

    stack php: comments

--- stack-php/stack-php.sh
+++ stack-php/stack-php.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 ######## IMDEMPOTENCY ########
 
+# docker ps
+# -q: only display hash
+# --filter name=stack-php: filter by name
 [[ -z $(docker ps -q --filter name=stack-php) ]] || docker rm -f $(docker ps -q --filter name=stack-php)
 
 ######## CONTAINERS ########
 
+# --name: container name
+# -d: detached mode
+# --restart unless-stopped: restart policy
+# -p 8080:80 : publish port 80 of container to port 8080 of every interfaces of host
 docker run \
        --name stack-php-nginx \
        -d --restart unless-stopped \
